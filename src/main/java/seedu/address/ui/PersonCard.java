a508f03cfabb5bfaeafb23f9a049b4a4c1bcdf6e
ama-chi <89921925+ama-chi@users.noreply.github.com>
2022-09-22 19:06:23 +0800

    Style 2

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -23,12 +23,10 @@ public class PersonCard extends UiPart<Region> {
      *
      * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
      */
+    public final Person person;
 
     @FXML
     private Label remark;
-
-    public final Person person;
-
     @FXML
     private HBox cardPane;
     @FXML
